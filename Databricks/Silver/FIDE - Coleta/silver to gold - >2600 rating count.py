cb75eac18ca3426a2449ca0d8a38f87ae90409e2
jpedrouerj@gmail.com <jpedrouerj@gmail.com>
2024-06-18 18:00:25 +0000

    atualização

--- Databricks/Silver/FIDE - Coleta/silver to gold - >2600 rating count.py
+++ Databricks/Silver/FIDE - Coleta/silver to gold - >2600 rating count.py
@@ -16,17 +16,14 @@ awesome_rated_players.printSchema()
 
 from pyspark.sql import functions as F
 
-# Filtra jogadores com rating maior que 2600
 filtered_df_rating = awesome_rated_players.filter(awesome_rated_players.rating > 2600)
 
-# Filtra a coluna 'flag' para remover registros com valor 'i'
 filtered_df_rating2 = filtered_df_rating.filter(filtered_df_rating.flag.isNull())
 
-# Agrupa por país e conta os jogadores
 awesome_rated_players_by_country = filtered_df_rating2.groupBy('country').count()
 awesome_rated_players_by_country2 = awesome_rated_players_by_country.withColumnRenamed("count", "player_count")
 
-# Exibe o resultado
+
 display(awesome_rated_players_by_country2)
 
 
